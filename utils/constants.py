9ab1ab35b88a474d80d1a4c17e990b67b9db0fba
Алекса <cetkovic@protonmail.com>
2024-02-01 12:49:10 +0100

    Add util which loads and prepares dataset

---
+++ utils/constants.py
@@ -0,0 +1,41 @@
+
+DATA_FILE = 'data\main-table.csv'
+
+COLS = {
+    'Region', 'Year', 'TotalTourists', 'ForeignTourists', 'HousesWithInternet%',
+    'CompaniesWithWebSites%', 'AmountOfPeopleWithHighEducation',
+    'PeopleWithHighEducationWithoutJob', 'AmountOfPeopleWithSecondaryEducation',
+    'PeopleWithSecondaryEducationWithoutJob', 'AmountOfPeopleWithoutEducation',
+    'PeopleWithoutEducationWithoutJob', 'AmountOfMales',
+    'AmountOfMalesWithoutJob', 'AmountOfFemales',
+    'AmountOfFemalesWithoutJob', 'AmountOfPeopleInRegion',
+    'AmountOfPeopleInRegionWithoutJob', 'AmountOf_15-24_PeopleInRegion',
+    'AmountOf_15-24_PeopleInRegionWithoutJob', 'AmountOf_15-64_PeopleInRegion',
+    'AmountOf_15-64_PeopleInRegionWithoutJob', 'AmountOf_25-34_PeopleInRegion',
+    'AmountOf_25-34_PeopleInRegionWithoutJob', 'AmountOf_35-44_PeopleInRegion',
+    'AmountOf_35-44_PeopleInRegionWithoutJob', 'AmountOf_45-54_PeopleInRegion',
+    'AmountOf_45-54_PeopleInRegionWithoutJob', 'AmountOf_55-64_PeopleInRegion',
+    'AmountOf_55-64_PeopleInRegionWithoutJob', 'AmountOf_65+_PeopleInRegion'
+}
+
+DEPENDENT_COL = 'TotalTourists'
+
+INDEX_COLS = 'Region', 'Year'
+
+INDEPENDENT_COLS = COLS - set(INDEX_COLS) - {DEPENDENT_COL}
+
+REGIONS = { 
+    'РЕПУБЛИКА СРБИЈА',
+    'Београдски регион', 
+    'Регион Војводине', 
+    'Регион Шумадије и Западне Србије', 
+    'Регион Јужне и Источне Србије' 
+}
+
+# REGION: Literal[
+#     'РЕПУБЛИКА СРБИЈА' | 
+#     'Београдски регион' | 
+#     'Регион Војводине' | 
+#     'Регион Шумадије и Западне Србије' | 
+#     'Регион Јужне и Источне Србије'
+# ] = 'РЕПУБЛИКА СРБИЈА'
---
+++ utils/utils.py
@@ -0,0 +1,42 @@
+from typing import Tuple
+import pandas as pd
+
+from constants import (
+    DATA_FILE, COLS, INDEX_COLS
+)
+
+
+
+
+def get_dataset() -> Tuple[pd.DataFrame, pd.DataFrame]:
+    
+    dataset = pd.read_csv(DATA_FILE, usecols=COLS, index_col=INDEX_COLS)
+    
+    mask = dataset.index.get_level_values('Region') == 'РЕПУБЛИКА СРБИЈА'
+    serbia = dataset[mask].droplevel('Region')
+
+    years = dataset.index.get_level_values('Year').to_list()
+    dataset['Year'] = pd.Categorical(years)
+
+    regions = dataset[~mask].groupby('Region').apply(lambda group: group.droplevel('Region'))
+
+    return regions, serbia
+
+
+
+if __name__ == "__main__":
+    # quick demo
+    regions_df, serbia_df = get_dataset()
+
+    if "serbia df demo":
+        print(serbia_df.head(), serbia_df.index) # data frame containing data for every year as index for Serbia
+        #print(serbia_df.loc[2020].head(7)) # get all cols for year
+        print(serbia_df.loc[2012:2015]) # loc[[2012, 2013, 2014, 2015]]
+        print("Total tourists in 2020: ", serbia_df.loc[2020, 'TotalTourists'])
+
+    if "regions df demo":
+        print(regions_df.head()) # data frame indexed by region and year
+        print("Belegrade data all years:\n", regions_df.loc["Београдски регион"].head())
+        print("Belegrade data 2014: \n", regions_df.loc["Београдски регион", 2014].head())
+        print("Belegrade data for years 2013-2017: \n", regions_df.loc["Београдски регион"].loc[2013:2017]) # can be done with one loc but idk how :)
+        print("All regions: ", regions_df.index.unique('Region').to_list())
