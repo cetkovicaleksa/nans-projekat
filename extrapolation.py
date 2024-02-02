66e2982613eb640c6d24169d8ce251d4e0d9a48a
maxim <mprivalovs@gmail.com>
2024-02-02 17:53:37 +0100

    Exploration

--- extrapolation.py
+++ extrapolation.py
@@ -15,6 +15,49 @@ columns_to_analyze = ["AmountOfPeopleWithHighEducation", "PeopleWithHighEducatio
                         "AmountOf_35-44_PeopleInRegionWithoutJob", "AmountOf_45-54_PeopleInRegion",
                         "AmountOf_45-54_PeopleInRegionWithoutJob", "AmountOf_55-64_PeopleInRegion",
                         "AmountOf_55-64_PeopleInRegionWithoutJob", "AmountOf_65+_PeopleInRegion"]
+import pandas as pd
+
+def fill_missing_values(column):
+    column = column.copy()  # Создаем явную копию столбца
+    for i in range(1, len(column) - 1):
+        if pd.isna(column.iloc[i]):
+            column.iloc[i] = column.iloc[i-1] - (column.iloc[i+1] - column.iloc[i-1])
+    return column
+
+# Ваш код для чтения данных
+df = pd.read_csv('data/main-table.csv')
+
+grouped_dfs = df.groupby('Region')
+region_dfs = []
+for region in regions:
+    if region in grouped_dfs.groups:
+        region_dfs.append(grouped_dfs.get_group(region))
+print(region_dfs)
+
+# Заполняем пропущенные значения согласно вашей формуле
+for col in columns_to_analyze:
+    for region_df in region_dfs:
+        region_df[col] = region_df[col].apply(fill_missing_values)
+
+# Создаем новый DataFrame с интерполированными значениями
+interpolated_dfs = []
+for region_df in region_dfs:
+    interpolated_df = region_df.drop('Region', axis=1).interpolate(method='linear', limit_direction='forward', axis=0).ffill().bfill()
+    interpolated_dfs.append(pd.concat([region_df['Region'], interpolated_df], axis=1))
+
+# Сохраняем результат в новый CSV-файл
+result_df = pd.concat(interpolated_dfs)
+result_df.to_csv('data/interpolated_data.csv', index=False)
+
+#
+# interpolated_dfs = []
+#
+# for region_df in region_dfs:
+#     interpolated_df = region_df.drop('Region', axis=1).interpolate(method='linear', limit_direction='forward', axis=0).ffill().bfill()
+#     interpolated_dfs.append(pd.concat([region_df['Region'], interpolated_df], axis=1))
+#     # interpolated_dfs.append(interpolated_df)
+#
+# print(interpolated_dfs)
 
 # regions = df['Region'].unique()
 #
@@ -32,23 +75,6 @@ columns_to_analyze = ["AmountOfPeopleWithHighEducation", "PeopleWithHighEducatio
 #
 # # Сохраняем обновленные данные в новый CSV-файл
 # df.to_csv('extrapolated_data_by_region.csv', index=False)
-
-grouped_dfs = df.groupby('Region')
-region_dfs = []
-for region in regions:
-    if region in grouped_dfs.groups:
-        region_dfs.append(grouped_dfs.get_group(region))
-print(region_dfs)
-
-interpolated_dfs = []
-
-for region_df in region_dfs:
-    interpolated_df = region_df.drop('Region', axis=1).interpolate(method='quadratic', limit_direction='forward', axis=0).ffill().bfill()
-    interpolated_dfs.append(pd.concat([region_df['Region'], interpolated_df], axis=1))
-    # interpolated_dfs.append(interpolated_df)
-
-print(interpolated_dfs)
-
 # .interpolate(method='linear', limit_direction='forward', axis=0)
 # df['Region'] = pd.Categorical(df['Region'])
 # #
